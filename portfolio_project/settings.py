1d26e4c9e60f25b6eaf6e63fc223138ebaa6b1ad
niharikaaaaa21 <167403357+niharikaaaaa21@users.noreply.github.com>
2024-11-09 17:32:30 +0530

    Update settings.py

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -39,12 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'portfolio',
 ]
-import os
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -120,8 +114,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+import os
+STATIC_URL = '/static/'
 
-STATIC_URL = 'static/'
+# Add the directory where your static files are stored (usually within your project root or an app)
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # Make sure the static folder exists at the root of your project
+]
+
+# Where collectstatic will put all static files for deployment
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
